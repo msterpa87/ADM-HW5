4130a9d93c8d0838751495d1fd6aa5107f57fe54
msterpa87 <marcosterpa1@gmail.com>
2021-01-14 10:59:40 +0100

    homework finished

--- graph.py
+++ graph.py
@@ -2,8 +2,13 @@ from collections import defaultdict, Counter
 import pandas as pd
 import seaborn as sns
 import numpy as np
+from tqdm import tqdm
+from contextlib import suppress
+from scipy import sparse
 
 sns.set_style("darkgrid")
+graph_filename = "wikigraph_reduced.csv"
+categories_filename = "reduced_categories.txt"
 
 
 def intersection(list_a, list_b):
@@ -12,28 +17,42 @@ def intersection(list_a, list_b):
 
 class Graph(object):
     def __init__(self):
+        # the following data structures are used to make the specific tasks (RQs)
+        # more time efficient, this of course at the expense of space efficiency.
         self.edge_list = []
+        self.node_list = set()
         self.categories = defaultdict(list)
         self.in_edge_dict = defaultdict(list)
         self.out_edge_dict = defaultdict(list)
         self.directed_bool = None
 
-        # categories
+        # the following data structures are used to manage labels (categories) of nodes
         self.category_dict = defaultdict(int)  # {category_str : id}
         self.nodes_by_category = defaultdict(list)  # {category_id : [node1, node2, ...]}
         self.category_by_node = defaultdict(int)  # {node : category_id}
 
     # ----------- GRAPH GENERATION ----------- #
 
-    def create_from_file(self, filename, sep="\t", cols=(1, 2)):
+    def create_from_file(self, filename=graph_filename, sep="\t", cols=(1, 2)):
         """ Creates a graph reading the edgelist from filename
-            expected rows format: row_n \t node \t node """
+
+        :param filename: str
+        :param sep: str
+        :param cols: list of ints
+        :return: None
+        """
+        # the expected format is that each row represents an edge with node labels
+        # found in cols separated by sep
         df = pd.read_csv(filename, sep=sep, usecols=cols)
         edge_list = df.values.tolist()
         self.create_from_edgelist(edge_list)
 
     def create_from_edgelist(self, edge_list):
-        """ Builds a graph object from an edge list [[u,v], ...] """
+        """ Populates the graph adding nodes and edges from edge_list
+
+        :param edge_list: list of (int, int)
+        :return: None
+        """
         edge_list = [(e[0], e[1]) for e in edge_list]
         self.edge_list = edge_list
 
@@ -41,11 +60,13 @@ class Graph(object):
         for e in edge_list:
             self.out_edge_dict[e[0]].append(e[1])
             self.in_edge_dict[e[1]].append(e[0])
+            self.node_list.add(e[0])
+            self.node_list.add(e[1])
 
         # to make successive calls to is_directed() more efficient
         self.directed_bool = None
 
-    def load_categories(self, filename):
+    def load_categories(self, filename=categories_filename):
         """ Loads nodes categories from filename, expected format:
             category: node1 node2 ..."""
         idx = 0
@@ -62,17 +83,84 @@ class Graph(object):
 
     # ----------- GENERAL PURPOSE ----------- #
 
+    # The following functions have been implemented in the process of solving
+    # specific RQs but were ultimately not used in the final solution
+    def add_node(self, node=None):
+        """ Add the specified node to the graph if possible, or adds a node
+            with an increasing label wrt to the max current label
+
+        :param node: int
+                     if None the node takes the largest index in the graph + 1
+        :return: None
+        """
+        if node is None:
+            node = max(self.node_list) + 1
+            self.node_list.add(node)
+            return node
+        elif node not in self.node_list:
+            self.node_list.add(node)
+        else:
+            print("[Error] Node already in the graph.")
+
+    def add_edge(self, edge):
+        """ Adds an edge to the graph.
+            The current implementation does not support parallel edges (i.e. multigraph)
+
+        :param edge: (int, int)
+        :return: None
+        """
+        assert(len(edge) == 2)
+        if edge in self.edge_list:
+            print("[Error] Edge already in the graph.")
+        else:
+            self.out_edge_dict[edge[0]].append(edge[1])
+            self.in_edge_dict[edge[1]].append(edge[0])
+
+    def delete_node(self, node):
+        """ Deletes the target node and all its incident edges from the graph
+
+        :param node: int
+        :return: None
+        """
+        if node not in self.node_list:
+            print("[Error] Specified node not in the graph.")
+            return None
+
+        with suppress(KeyError, ValueError):
+            # delete incoming edges
+            for u in self.in_edge_dict[node]:
+                self.out_edge_dict[u].remove(node)
+                self.edge_list.remove((u, node))
+            # delete outcoming edges
+            for u in self.out_edge_dict[node]:
+                self.in_edge_dict[u].remove(node)
+                self.edge_list.remove((node, u))
+            # delete node
+            del self.in_edge_dict[node]
+            del self.out_edge_dict[node]
+            self.node_list.remove(node)
+
+    def delete_edge(self, edge):
+        if edge not in self.edge_list:
+            print("Specified edge not in the graph.")
+            return None
+
+        with suppress(KeyError, ValueError):
+            self.in_edge_dict[edge[1]].remove(edge[0])
+            self.out_edge_dict[edge[0]].remove(edge[1])
+            self.edge_list.remove(edge)
+
     def edge_count(self):
         """ Returns the number of edges in G """
         return len(self.edge_list)
 
     def node_count(self):
         """ Returns the number of nodes in G """
-        return len(self.out_edge_dict.keys())
+        return len(self.node_list)
 
     def max_node_id(self):
         """ Returns the maximum id of nodes in the graph """
-        return max([max(u,v) for u,v in self.edge_list])
+        return max([max(u, v) for u, v in self.edge_list])
 
     def is_directed(self):
         """ Returns True if G is directed, False otherwise """
@@ -224,27 +312,6 @@ class Graph(object):
 
         return list(visited.keys())
 
-    def distance_vector(self, root):
-        """ Returns the distance vector of each node from root """
-        visited = defaultdict(bool)  # boolean vector of visits
-        frontier = defaultdict(list)
-        frontier[1] = self.neighbours(root)
-        i = 1
-        distances = dict()
-
-        while True:
-            if len(frontier[i]) == 0:
-                break
-            for u in frontier[i]:
-                distances[u] = i
-                for v in self.neighbours(u):
-                    if not visited[v]:
-                        visited[v] = True
-                        frontier[i + 1].append(v)
-            i += 1
-
-        return distances
-
     # ---------------- RQ 3 ---------------- #
 
     def reachable_targets(self, root, targets, dist):
@@ -257,7 +324,7 @@ class Graph(object):
 
     # ---------------- RQ 4 ---------------- #
 
-    def modified_dfs(self, node, target, path, visited_edges):
+    def modified_dfs(self, node, target, visited_edges):
         """
 
         :param node:
@@ -266,29 +333,32 @@ class Graph(object):
         :param visited_edges:
         :return:
         """
+        if node == target:
+            return True, visited_edges
+
         next_nodes = self.neighbours(node)
-        if target in next_nodes:
-            path.append(node)
-            visited_edges[(node, target)] = True
-            return True, path, visited_edges
-        else:
-            for u in next_nodes:
-                if u in path or visited_edges[(node, u)]:
-                    continue
-                else:
-                    flag, path, visited_edges = self.modified_dfs(u, target, path, visited_edges)
-                    if flag:
-                        return True, path, visited_edges
-        return False, path, visited_edges
+        for u in next_nodes:
+            if visited_edges[(node, u)]:
+                continue
+
+            # mark next node as visited
+            visited_edges[(node, u)] = True
+            # visit next node
+            flag, visited_edges = self.modified_dfs(u, target, visited_edges)
+            if flag:
+                return True, visited_edges
+
+        return False, visited_edges
 
     def mincut_between_nodes(self, source, target):
         """ Returns the set of nodes at distance at most dist from root """
         visited_edges = defaultdict(bool)  # boolean vector of visits
-        path = [source]
         count = 0
 
         for node in self.neighbours(source):
-            flag, path, visited_edges = self.modified_dfs(node, target, path, visited_edges)
+            if visited_edges[(source, node)]:
+                continue
+            flag, visited_edges = self.modified_dfs(node, target, visited_edges)
             if flag:
                 count += 1
 
@@ -296,24 +366,48 @@ class Graph(object):
 
     # ---------------- RQ 5 ---------------- #
 
-    def get_closest_categories(self, cat_id):
+    def distance_vector(self, root):
+        """ Returns the distance vector of each node from root """
+        visited = defaultdict(bool)  # boolean vector of visits
+        frontier = defaultdict(list)
+        frontier[1] = self.neighbours(root)
+        distances_by_cat = defaultdict(list)
+        i = 1
+        distances = dict()
+
+        while True:
+            if len(frontier[i]) == 0:  # no more nodes to visit
+                break
+            for u in frontier[i]:
+                distances[u] = i
+
+                for v in self.neighbours(u):
+                    if not visited[v]:
+                        visited[v] = True
+                        frontier[i + 1].append(v)
+            i += 1
+
+        return distances
+
+    def get_closest_categories(self, source_cat_id):
         """ Returns the list of categories sorted by their distance from cat_id """
         distances_by_cat = defaultdict(list)
 
-        for node in self.get_nodes_from_category_id(cat_id):
+        for node in tqdm(self.get_nodes_from_category_id(source_cat_id)):
             # vector of distances between each vertex and u
             distances = self.distance_vector(node)
 
-            for u, v in distances.items():
-                target_cat_id = self.get_node_category(u)
-                distances_by_cat[target_cat_id].append(v)
+            for target_node, dist in distances.items():
+                # save node distance grouped by category
+                target_cat_id = self.get_node_category(target_node)
+                distances_by_cat[target_cat_id].append(dist)
 
         medians = dict()
 
-        for cat_id, dist_list in distances_by_cat.items():
-            medians[cat_id] = np.median(dist_list)
+        for target_cat_id, dist_list in distances_by_cat.items():
+            medians[target_cat_id] = np.median(dist_list)
 
-        del medians[cat_id]  # remove source category
+        del medians[source_cat_id]  # remove source category
 
         sorted_cat = sorted(zip(self.categories_list(), medians.values()),
                             key=lambda x: x[1])
@@ -322,7 +416,10 @@ class Graph(object):
     # ---------------- RQ 6 ---------------- #
 
     def get_category_subgraph(self):
-        """ Returns the graph representing the connection between categories """
+        """ Returns the graph representing the connection between categories
+
+        :return:
+        """
         edge_list = set()
 
         for u, v in self.edge_list:
@@ -333,9 +430,19 @@ class Graph(object):
         return subgraph
 
     def get_adjacency_matrix(self, normalize=False):
-        """ Returns a numpy adjacency matrix """
+        """ Returns a numpy adjacency matrix
+
+        :param normalize: bool
+                          if True normalizes each row of the matrix
+                          useful when called by pagerank()
+        :return:
+        """
         n = self.max_node_id() + 1
-        M = np.identity(n)
+        try:
+            M = np.identity(n)
+        except MemoryError:
+            print("[Memory Error] Underlying matrix too large.")
+            return None
 
         for u, v in self.edge_list:
             M[u, v] = 1
@@ -347,11 +454,15 @@ class Graph(object):
         return M
 
     def pagerank(self, n_iter=100, d=.85):
-        """
+        """ Returns the pagerank ranking of the nodes
+            The code is an adaptation of the one found on
+            https://en.wikipedia.org/wiki/PageRank
 
-        :param n_iter:
-        :param d:
-        :return:
+        :param n_iter: int
+                       maximum number of iterations for convergence
+        :param d: float
+                  damping factor
+        :return: list of floats
         """
         n = self.max_node_id() + 1
         M = self.get_adjacency_matrix(normalize=True)
@@ -363,6 +474,17 @@ class Graph(object):
             v = M_hat @ v
 
         return v
+
+    def sort_category_by_pagerank(self):
+        """ Returns the list of categories sorted according to their pagerank score
+
+        :return: list of strings
+        """
+        g_cat = self.get_category_subgraph()
+        pr = g_cat.pagerank()
+        categories = self.categories_list()
+        sorted_categories = sorted(zip(categories, pr), key=lambda x: x[1], reverse=True)
+        return [x[0] for x in sorted_categories]
 
     # ---------------- RQ 1 ---------------- #
 
